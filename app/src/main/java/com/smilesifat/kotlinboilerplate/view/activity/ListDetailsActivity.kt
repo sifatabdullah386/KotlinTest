9ebafd65e92b848568ba5912f00505c939ff5bbe
Md. Abdullah Al Sifat <sifatabdullah386@gmail.com>
2023-12-22 17:37:08 +0600

    Details Updated

--- app/src/main/java/com/smilesifat/kotlinboilerplate/view/activity/ListDetailsActivity.kt
+++ app/src/main/java/com/smilesifat/kotlinboilerplate/view/activity/ListDetailsActivity.kt
@@ -6,8 +6,6 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.request.RequestOptions
 import com.smilesifat.kotlinboilerplate.R
 
 class ListDetailsActivity : AppCompatActivity() {
@@ -15,20 +13,21 @@ class ListDetailsActivity : AppCompatActivity() {
     var id: String? = null
     var name: String? = null
     var full_name: String? = null
-    var id: String? = null
-    var id: String? = null
-    var id: String? = null
-    var id: String? = null
+    var description: String? = null
+    var avatar: String? = null
+    var url: String? = null
+    var created_at: String? = null
+    var language: String? = null
 
 
 
-    var image: ImageView? = nullgit
-    var name: TextView? = null
-    var full_name: TextView? = null
-    var description: TextView? = null
-    var url: TextView? = null
-    var created_at: TextView? = null
-    var language: TextView? = null
+    var imageView: ImageView? = null
+    var nameView: TextView?=null
+    var full_nameView: TextView? = null
+    var descriptionView: TextView? = null
+    var urlView: TextView? = null
+    var created_atView: TextView? = null
+    var languageView: TextView? = null
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,20 +43,29 @@ class ListDetailsActivity : AppCompatActivity() {
         language = intent.getStringExtra("language")
         created_at = intent.getStringExtra("created_at")
 
-        image = findViewById<ImageView>(R.id.ad_image)
-        name = findViewById<TextView>(R.id.name)
-        full_name = findViewById<TextView>(R.id.full_name)
-        description = findViewById<TextView>(R.id.description)
-        url = findViewById<TextView>(R.id.url)
-        created_at = findViewById<TextView>(R.id.created_at)
-        language = findViewById<TextView>(R.id.language)
+        imageView = findViewById<ImageView>(R.id.ad_image)
+        nameView = findViewById<TextView>(R.id.name)
+        nameView?.text = name
 
+        full_nameView = findViewById<TextView>(R.id.full_name)
+        full_nameView?.text = full_name
+        descriptionView = findViewById<TextView>(R.id.description)
+        descriptionView?.text = description
+        urlView = findViewById<TextView>(R.id.url)
+        urlView?.text = url
 
-        if (avatar.isNotEmpty()) {
-            val into = Glide.with().load(avatar).apply(
-                RequestOptions().placeholder(R.drawable.no_image_found)
-                    .diskCacheStrategy(DiskCacheStrategy.ALL).error(R.drawable.no_image_found)
-            ).into(image)
+        created_atView = findViewById<TextView>(R.id.created_at)
+        created_atView?.text = created_at
+
+        languageView = findViewById<TextView>(R.id.language)
+        languageView?.text = language
+
+        if (avatar!!.isNotEmpty()) {
+            imageView?.let {
+                Glide.with(applicationContext)
+                    .load(avatar)
+                    .into(it)
+            }
         }
 
     }
